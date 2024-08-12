4292ea151af07637104b96011d72b658eef1e414
WRXinYue <3322543587@qq.com>
2024-08-12 16:20:24 +0800

    chore(deps): upgrade valaxy to v0.19.7

--- ocean/layouts/post.vue
+++ ocean/layouts/post.vue
@@ -5,23 +5,21 @@ const isHome = useLayout('home')
 </script>
 
 <template>
-  <Layout>
-    <SakuraMultiColumnsLayout class="sidebar">
-      <template #left>
-        <SakuraAsideLayout>
-          <SakuraSidebarCustom />
-        </SakuraAsideLayout>
-      </template>
-      <template #content>
-        <SakuraArticle>
-          <RouterView />
-        </sakuraarticle>
-      </template>
-      <template #right>
-        <SakuraAsideLayout>
-          <SakuraToc v-if="!isHome" />
-        </SakuraAsideLayout>
-      </template>
-    </SakuraMultiColumnsLayout>
-  </Layout>
+  <SakuraMultiColumnsLayout class="sidebar">
+    <template #left>
+      <SakuraAsideLayout>
+        <SakuraSidebarCustom />
+      </SakuraAsideLayout>
+    </template>
+    <template #content>
+      <SakuraArticle>
+        <RouterView />
+      </sakuraarticle>
+    </template>
+    <template #right>
+      <SakuraAsideLayout>
+        <SakuraToc v-if="!isHome" />
+      </SakuraAsideLayout>
+    </template>
+  </SakuraMultiColumnsLayout>
 </template>
